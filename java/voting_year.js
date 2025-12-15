81c6af50fe52fb14db8a3077462dd506864364e2
cjaye-dif019 <b2029cjadifuntorum@pshs.edu.ph>
2025-12-15 23:12:35 +0800

    teacher year list

--- java/voting_year.js
+++ java/voting_year.js
@@ -2,110 +2,94 @@
 const teacherYear = {
   //FYP
 
-  // biology
-  "Dawn Crisologo":"FYP","Carmina Dalida":"FYP","Jerrica Alzate-Gunda":"FYP",
-  // chemistry
-  "Lovelyn Balbedina":"FYP", "Mara Esguerra":"FYP","Jayson Bingcang":"FYP",
-  // computer science
-  "Maria Capundag":"FYP","Lodivina Pabello":"FYP","Jonathan Javier":"FYP",
-  //english
-  "Recel Calaunan":"FYP","Edwin Del Rosario":"FYP","Sheila Fernandez":"FYP", "Liz Saguico":"FYP",
-  //filipino
-  "China De Vera":"FYP","Lerma Espina":"FYP",
-  //integrated science/earth science
-  "Mike Aloba":"FYP","Rey Nino Baguio":"FYP","Princess Casa":"FYP","Bernard Llaguno":"FYP", "Yna Mongcal":"FYP","Deanne Zambrano":"FYP", 
+  //bio
+  "Dawn Crisologo":"FYP", "Carmina Dalida":"FYP", "Jerrica Alzate-Gunda":"FYP",
+  //chem
+  "Lovelyn Balbedina":"FYP", "Mara Esguerra":"FYP", "Jayson Bingcang":"FYP",
+  //cs
+  "Maria Capundag":"FYP", "Lodivina Pabello":"FYP", "Jonathan Javier":"FYP",
+  //eng
+  "Recel Calaunan":"FYP", "Edwin Del Rosario":"FYP", "Sheila Fernandez":"FYP", "Liz Saguico":"FYP",
+  //fil
+  "China De Vera":"FYP", "Lerma Espina":"FYP",
+  //i/es
+  "Mike Aloba":"FYP", "Rey Nino Baguio":"FYP", "Princess Casa":"FYP", "Bernard Llaguno":"FYP", "Yna Mongcal":"FYP", "Deanne Zambrano":"FYP", 
   //math
-  "Franel Infante": "FYP","Mardan Llanura": "FYP","Louvelle Parulan": "FYP", "Melodee Pacio": "FYP", "Karlo Kristobal": "FYP", "Lieza Crisostomo": "FYP",
-  "Maria Torralba": "FYP",
-  "Glenn Espino": "FYP & AYP",     
-  "Arnold Lapuz": "FYP",           
-  "Erin Dela Cruz": "FYP",         
-  "Kim Favor": "FYP & AYP",   
-   "Renan Ortiz": "FYP",
-  "Ervin Santos": "FYP",
-  "Boon Lauw": "FYP",
-  "Monica Xavier": "FYP",
-  "Hati Callo":"FYP",
-    "Fil Teodoro":"FYP",
-    "Tolits Englatera":"FYP",
-    "Melody Hernandez":"FYP",
-    "Eileen Sarmago":"FYP",
-    "Natalie Basquez":"FYP",
-    "Jane Turingan":"FYP",
+  "Franel Infante": "FYP", "Mardan Llanura": "FYP", "Louvelle Parulan": "FYP", "Melodee Pacio": "FYP", "Karlo Kristobal": "FYP", 
+  //p6
+  "Lieza Crisostomo": "FYP", "Maria Torralba": "FYP",
+  //ss
+  "Glenn Espino": "FYP & AYP", "Arnold Lapuz": "FYP", "Erin Dela Cruz": "FYP", "Kim Favor": "FYP & AYP",   
+  //tech
+  "Renan Ortiz": "FYP", "Ervin Santos": "FYP", "Boon Lauw": "FYP", "Monica Xavier": "FYP",
+  //pehm
+  "Hati Callo":"FYP", "Fil Teodoro":"FYP", "Tolits Englatera":"FYP", "Melody Hernandez":"FYP",
+  //ve
+  "Eileen Sarmago":"FYP", "Natalie Basquez":"FYP", "Jane Turingan":"FYP",
 
 
+  //AYP
 
-  "Neil Ramirez":"AYP","Maria Pinlac":"AYP","Melanie Cheng":"AYP",
-  "Aileen Dela Cruz":"AYP","Kris Aquino":"AYP", "Lester Mendoza":"AYP", "Kat Villar":"AYP",
-  "Joseph Estrella":"AYP", "Roy Canseco":"AYP", "Aline Mendoza":"AYP","Phillip Lim":"AYP",
-  "Mark Tarcelo":"AYP","Joanna Soriano":"AYP","Jez Ambrocio":"AYP","Benigno Montemayor":"AYP",
-  "Kornellie Raquitico":"AYP","Camille Barquilla":"AYP",
-  "Simoun Monzon":"AYP","Ligaya Cion":"AYP","Francis Orque":"AYP",  "Kiel Granada": "AYP",
-  "Mary Magpantay": "AYP",
-  "Von Chua": "AYP",
-  "Diane Estonilo": "AYP",
-  "Darlene Gustilo": "AYP",
-  "Jayson Palomar": "AYP",
-  "Fortunato Tacuboy": "AYP & SYP",
-  "Neil Bartolay": "AYP",
-  "Anna Bonifacio": "AYP",
-  "Marvice Ong": "AYP",
-  "Hazel Manuel": "AYP",
-   "Charles De Guzman": "AYP",      
-  "Diana Sarenas": "AYP",         
-    "Rose Mary Butaran": "AYP",
-  "Ana Chupungco": "AYP",
-  "Kresta David": "AYP",
-  "Benedict Mendoza": "AYP",
-    "Ryan Cruz": "AYP",
-      "JM Alvarez": "AYP",
-        "Robert Duliesco": "AYP",
-  "Jennifer Balangue": "AYP",
-    "Florides Anda": "AYP",
-
-      "Dianmar Briones": "AYP",
-  "Maybeline Pacia": "AYP",
+  //bio
+  "Neil Ramirez":"AYP", "Maria Pinlac":"AYP", "Melanie Cheng":"AYP",
+  //chem
+  "Aileen Dela Cruz":"AYP", "Kris Aquino":"AYP", "Lester Mendoza":"AYP", "Kat Villar":"AYP", "Joseph Estrella":"AYP", 
+  //cs
+  "Roy Canseco":"AYP", "Aline Mendoza":"AYP", "Phillip Lim":"AYP", "Mark Tarcelo":"AYP",
+  //eng
+  "Joanna Soriano":"AYP", "Jez Ambrocio":"AYP", "Benigno Montemayor":"AYP", "Kornellie Raquitico":"AYP",
+  //fil
+  "Camille Barquilla":"AYP", "Simoun Monzon":"AYP", "Ligaya Cion":"AYP", "Francis Orque":"AYP",  
+  //math & stat
+  "Kiel Granada": "AYP", "Mary Magpantay": "AYP", "Von Chua": "AYP", "Diane Estonilo": "AYP", "Darlene Gustilo": "AYP", "Jayson Palomar": "AYP", "Fortunato Tacuboy": "AYP & SYP",
+  //p6
+  "Neil Bartolay": "AYP", "Anna Bonifacio": "AYP", "Marvice Ong": "AYP", "Hazel Manuel": "AYP",
+  //ss
+  "Charles De Guzman": "AYP", "Diana Sarenas": "AYP",        
+  //research
+  "Rose Mary Butaran": "AYP", "Ana Chupungco": "AYP", "Kresta David": "AYP", "Benedict Mendoza": "AYP",
+  //pehm
+  "Ryan Cruz": "AYP", "JM Alvarez": "AYP", "Robert Duliesco": "AYP", "Jennifer Balangue": "AYP", 
+  //ve
+  "Florides Anda": "AYP", "Dianmar Briones": "AYP", "Maybeline Pacia": "AYP",
 
 
+  //SYP
 
+  //bio
+  "Christian Abagat":"SYP","Rieziel Bernal":"SYP", "Ralph Hipolito":"SYP", "Danah Purificacion":"SYP", "Chuckie Calsado":"SYP", "Felipe Del Castillo":"SYP", "Justin Guce":"SYP",
+  //chem
+  "Dinnie Morales":"SYP", "Azlek Tan":"SYP", "Leonel Po":"SYP", "Xandra Razonabe":"SYP", "Arianne Won":
+  //cs
+  "SYP","Edge Angeles":"SYP",
+  //eng
+  "Kim De Castro":"SYP", "Jennifer Bermudez":"SYP", "Eric Nebran":"SYP",
+  //fil
+  "Christine Aguila":"SYP", "Mark Lopez":"SYP", "Lilibeth Quiore":"SYP",
+  //math 
+  "Mark Ayaay": "SYP", "Faith Daguman": "SYP", "Petri Espanol": "SYP", 
+  //p6
+  "Justin Alvarez": "SYP", "Bimbo Galit": "SYP", "Gerold Pedemonte": "SYP", "Bhazel Pelicano": "SYP", "Marc Perez": "SYP", "John Pingol": "SYP",
+  //ss
+  "Vladmir Lopez": "SYP", "Bambie Untalan-Mina": "SYP", "Sarah Eisma": "SYP",
+  //research
+  "Restituto Cortez": "SYP", "Elijah Estante": "SYP", "Donna Hipolito": "SYP", "Elliard Yanza": "SYP", "Catherine Dela Cruz": "SYP", "Alangelico San Pascual": "SYP", "Mc Villaruel": "SYP",
+  //tech
+  "Andrei Castro": "SYP", "Zebastian Dogma": "SYP", "John Carlo Elopre": "SYP", "Marivic Morata": "SYP", "Jason Alcarez": "SYP", "Jejomar Bongat": "SYP", "Ana Carvajal": "SYP",
 
-
-
-  "Christian Abagat":"SYP","Rieziel Bernal":"SYP",
-  "Ralph Hipolito":"SYP","Danah Purificacion":"SYP","Chuckie Calsado":"SYP","Felipe Del Castillo":"SYP",
-  "Justin Guce":"SYP","Dinnie Morales":"SYP", "Azlek Tan":"SYP",
-  "Leonel Po":"SYP","Xandra Razonabe":"SYP","Arianne Won":"SYP","Edge Angeles":"SYP","Kim De Castro":"SYP",
-  "Jennifer Bermudez":"SYP","Eric Nebran":"SYP","Christine Aguila":"SYP","Mark Lopez":"SYP","Lilibeth Quiore":"SYP","name":"SYP",
-    "Mark Ayaay": "SYP",
-  "Faith Daguman": "SYP",
-  "Petri Espanol": "SYP",
-    "Justin Alvarez": "SYP",
-  "Bimbo Galit": "SYP",
-  "Gerold Pedemonte": "SYP",
-  "Bhazel Pelicano": "SYP",
-  "Marc Perez": "SYP",
-  "John Pingol": "SYP",
-   "Vladmir Lopez": "SYP",          
-  "Bambie Untalan-Mina": "SYP",    
-  "Sarah Eisma": "SYP",
-    "Restituto Cortez": "SYP",
-  "Elijah Estante": "SYP",
-  "Donna Hipolito": "SYP",
-  "Elliard Yanza": "SYP",
-  "Catherine Dela Cruz": "SYP",
-  "Alangelico San Pascual": "SYP",
-  "Mc Villaruel": "SYP",
-  "Andrei Castro": "SYP",
-  "Zebastian Dogma": "SYP",
-  "John Carlo Elopre": "SYP",
-  "Marivic Morata": "SYP",
-    "Jason Alcarez": "SYP",
-  "Jejomar Bongat": "SYP",
-  "Ana Carvajal": "SYP",
-
-  "Efren Paz": "FYP & SYP","John Wyson":"FYP & SYP", "Mary Dino":"FYP & SYP", "Flordeliza Fermin":"FYP & SYP",   "Pao Apolinario": "FYP & SYP", 
-
-  "Mark Tarcelo": "FYP & AYP", "Nico Orduna": "FYP & AYP", 
-  "Edwin De Ocampo ": "AYP & SYP", "Joseph Estrella": "AYP & SYP", "Sarah Napoles": "AYP & SYP",  "Kim Gargar": "AYP & SYP"
-
+  
+  //mix
+  
+  //chem
+  "Efren Paz": "FYP & SYP", "Edwin De Ocampo ": "AYP & SYP", "Joseph Estrella": "AYP & SYP",
+  //eng
+  "John Wyson":"FYP & SYP", "Sarah Napoles": "AYP & SYP",
+  //fil
+  "Mary Dino":"FYP & SYP", "Flordeliza Fermin":"FYP & SYP", 
+  //math
+  "Pao Apolinario": "FYP & SYP", 
+  //p6
+  "Kim Gargar": "AYP & SYP",
+  //pehm
+  "Nico Orduna": "FYP & AYP"
 };
